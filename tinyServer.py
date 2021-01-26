e93e88df4bc427f6c0d0c4d2a8aa2defc6ee0e7f
rodartes <63331752+rodartes@users.noreply.github.com>
2021-01-25 23:22:51 -0800

    Update tinyServer.py

--- tinyServer.py
+++ tinyServer.py
@@ -15,14 +15,13 @@ message = "HTTP/1.1 200 OK\r\n"\
 
 #3. create and run socket - (referenced from https://docs.python.org/3/library/socket.html#example and https://realpython.com/python-sockets/#echo-client-and-server)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mySocket:
-   mySocket.bind((myHost, myPort))
-   mySocket.listen()
-   conn, addr = mySocket.accept()
-   with conn:
-    print('Connected by', addr)
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        conn.sendall(data)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
